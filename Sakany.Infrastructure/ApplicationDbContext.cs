a0fa9d5d3a0a27f554adb6946dc1ad11650cf2f4
Mousa <54138093+Mousa-Ahmed98@users.noreply.github.com>
2024-05-02 00:09:49 +0300

    Removed weather controller

--- Sakany.Infrastructure/ApplicationDbContext.cs
+++ Sakany.Infrastructure/ApplicationDbContext.cs
@@ -1,14 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.Identity;
 using Sakany.Core.Entities;
-using Sakany.Infrastructure.Configurations;
 using System.Reflection;
 
 
